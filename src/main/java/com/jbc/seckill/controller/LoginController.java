74dd1c6d3d33f5a6663960786e4b5c1aa0ba53b7
cjpfor <cjpfor@163.com>
2019-08-22 17:29:35 +0800

    add

--- src/main/java/com/jbc/seckill/controller/LoginController.java
+++ src/main/java/com/jbc/seckill/controller/LoginController.java
@@ -4,11 +4,14 @@ import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 import com.jbc.seckill.redis.RedisService;
+import com.jbc.seckill.result.CodeMsg;
 import com.jbc.seckill.result.Result;
+import com.jbc.seckill.vo.LoginVo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -32,11 +35,19 @@ public class LoginController {
     
     @RequestMapping("/do_login")
     @ResponseBody
-    public Result<Boolean> doLogin(HttpServletResponse response) {
+    public Result<Boolean> doLogin(LoginVo loginVo, HttpServletResponse response) {
     	/*log.info(loginVo.toString());
     	//登录
     	userService.login(response, loginVo);
     	return Result.success(true);*/
+    	String mobile = loginVo.getMobile();
+    	String password = loginVo.getPassword();
+    	if(StringUtils.isEmpty(mobile)){
+    	    return Result.error(CodeMsg.MOBILE_EMPTY);
+        }
+        if(StringUtils.isEmpty(password)){
+
+        }
 
         try {
             Thread.sleep(2000);
---
+++ src/main/java/com/jbc/seckill/vo/LoginVo.java
@@ -0,0 +1,22 @@
+package com.jbc.seckill.vo;
+
+public class LoginVo {
+    private String mobile;
+    private String password;
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
